5bafb399a5729eb5e1e4022ee17aaf8cdff0fd71
Carlos Avila <cavilart21@gmail.com>
2023-10-02 15:36:16 +0200

    remove netlify folder

--- .netlify/functions-internal/chunks/about_588b35ed.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/about_839fb7a9.mjs').then(n => n.b);
-
-export { page };
