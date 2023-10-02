e2ab932b2f636d9dc0c296153059326ea3f0f934
Carlos Avila <cavilart21@gmail.com>
2023-10-02 15:21:27 +0200

    adding neltify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: netlify(),
+  vite: {
+    ssr: {
+      noExternal: ["react-icons"],
+    },
+  },
+  output: "server",
 });
