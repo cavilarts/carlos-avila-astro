974ab5a769ca2a4cb32a1dda9d7b48343f442336
Carlos Avila <cavilart21@gmail.com>
2023-10-02 15:46:46 +0200

    renaming timeline

--- src/components/TL/WorkTL.tsx
+++ src/components/TL/WorkTL.tsx
@@ -4,7 +4,7 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
-export default function WorkTimeline() {
+export default function WorkTL() {
   return (
     <VerticalTimeline>
       <VerticalTimelineElement
